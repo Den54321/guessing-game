5771860d552cf60e56c3f07ef501b45ad183ea53
Den54321 <dennbuika@gmail.com>
2023-02-27 18:44:09 +0300

    docs: modified guessing-game.js file

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,28 @@
 class GuessingGame {
-    constructor() {}
-
-    setRange(min, max) {
-
+    constructor() {
+        this.maxi = 0;
+        this.mini = 0;
+        this.oldmaxi;
     }
+        setRange(min, max) {
+            this.maxi = max;
+            this.mini = min;
+        }
 
-    guess() {
+        guess(){
+            return   Math.ceil(this.mini +(this.maxi - this.mini)/2) ;
+        }
 
-    }
+        lower(){
+            this.maxi = this.maxi - Math.floor((this.maxi - this.mini)/2) ;
+        }
 
-    lower() {
+        greater(){
 
-    }
+            this.mini = this.mini +(this.maxi - this.mini)/2;  
+        }
 
-    greater() {
-
-    }
-}
+} 
+    
 
 module.exports = GuessingGame;
